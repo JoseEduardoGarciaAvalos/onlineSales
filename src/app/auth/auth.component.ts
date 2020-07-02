2fee1f595e3f2a12a4600028b008378b70601d48
Jose <email>
2020-07-02 07:18:53 -0600

    complete component Auth Module Root

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from "../auth.service" //37a
 
 @Component({
   selector: 'app-auth',
@@ -6,10 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./auth.component.css']
 })
 export class AuthComponent implements OnInit {
+  //37c
+  private login: string;
+  private pass: string;
 
-  constructor() { }
+  constructor(public authService: AuthService) {} //37b
 
   ngOnInit() {
   }
+  //37d
+  onSubmit() {
+    this.authService.isLogin = false;
+    this.authService.autenticar(this.login, this.pass).subscribe(res => {
+      let aux = res;
+      if ( aux.length > 0 ) {
+        this.authService.isLogin = true;
+        this.authService.cliente.nombre = aux[0];
+        this.authService.cliente.apellido = aux[1];
+        this.authService.cliente.email = this.login;
+      }
+    });
+  }
+
+  desconectar() {
+    this.authService.isLogin = false;
+}
 
 }
