5fb7dd51123fe643c701a130468320d77f78cde9
Jose <email>
2020-07-08 07:40:44 -0600

    complete component ProductoKeywords M Busqueda

--- src/app/busqueda/producto-keyword/producto-keyword.component.ts
+++ src/app/busqueda/producto-keyword/producto-keyword.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-producto-keyword',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductoKeywordComponent implements OnInit {
 
-  constructor() { }
+  constructor(private routher:Router) { }
 
   ngOnInit() {
+  }
+
+  productoSeleccion(terminos:string):void {
+    this.routher.navigate(["/busqueda", { outlets: { "listaBusqueda": ["keywords", terminos]} }]);
+    /*
+      Semgento de ruta   Declarada en
+      /busqueda         = app-routing.module.ts     path: "/busqueda"   (absoluta)  rootModule
+      ListaBusqueda     = app.component.html        <router-outlet name="listaBusqueda"></router-outlet>
+      keywords          = busqueda-routing.module.ts  path: "keywords"  (relativa)  featureModule
+
+      /busqueda/(listaBusqueda:keywords/terminos)
+    */
   }
 
 }
