86b4ebde745c20ece16c7ded9f5d51e69cd65520
Jose <email>
2020-07-10 06:56:00 -0600

    complete component ProductoLista M Busqueda

--- src/app/busqueda/producto-lista/producto-lista.component.ts
+++ src/app/busqueda/producto-lista/producto-lista.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from "@angular/router";
+import { BusquedaService } from "../busqueda.service";
 
 @Component({
   selector: 'app-producto-lista',
@@ -6,10 +8,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./producto-lista.component.css']
 })
 export class ProductoListaComponent implements OnInit {
+  public productos :Object[] = [];
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private busquedaService:BusquedaService ) { }
 
   ngOnInit() {
-  }
+    this.route.params.subscribe((parametros :Params) => {
+         let ruta = "";
+         if (parametros["tipo"] !== undefined) {
+             ruta = "criterio/"+parametros['tipo']+"/"+parametros['marca']+"/"+parametros['minprecrio']+"/"+parametros['maxprecio']+"/"+parametros['minpopularidad'];
+         }
+         else {
+             ruta = "keywords?"+parametros["terminos"].split(" ").join("&");
+         }
+
+         this.busquedaService.getProductos(ruta)
+             .subscribe(res => this.productos = res,
+                        err => console.error(err),
+                        () => console.log('getProductos (desde producto-lista) bien'));       
+                    
+         });
+ }
 
 }
